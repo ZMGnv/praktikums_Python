bf6f0b5120cb916fd6532efce318ccaf2c5d567f
JanisARubezis <73471045+JanisARubezis@users.noreply.github.com>
2021-05-05 21:05:19 +0300

    iesniedzu

--- uzd1.py
+++ uzd1.py
@@ -4,3 +4,13 @@ riņķa līnijas rādiusu un izvada uz ekrāna (print)
 riņķa līnijas garumu un laukumu, atbilstoši noformējot atbildi.
 Pārbaudiet programmas darbību ar dažādiem ievaddatiem.
 """
+import math
+
+rad=float(input("Ievadi rādiusu:"))
+garums=float(math.pi*rad*2)
+laukums=float(math.pi*(rad**2))
+
+print("Riņķa līnijas garums ir")
+print(garums)
+print("un riņķa laukums ir")
+print(laukums)
--- uzd5.py
+++ uzd5.py
@@ -3,3 +3,14 @@ Uzrakstiet programmu Python,
 kas pieņem veselu skaitli (n) un 
 aprēķina n + nn + nnn vērtību.
 """
+
+n=float(input("Ievadi vesalu skaitli:"))
+
+x=n+pow(n, 2)+pow(n, 3)
+print(x)
+"""
+def skaitlis(n):
+    y=n+pow(n, 2)+pow(n,3)
+    return y
+print(skaitlis(2))
+"""
